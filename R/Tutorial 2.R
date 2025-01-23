35aa01f512c7e7e310ac9db48ab0577a90271dff
Mehtab Hanzroh - SP6 <mehtabhanzroh@gmail.com>
2025-01-23 12:42:43 -0500

    Tutorial 2 R Code

--- R/Tutorial 2.R
+++ R/Tutorial 2.R
@@ -9,26 +9,26 @@ library(FNN)
 
 ### More on Linear Regression ###
 # Loading a dataset
-df = as.data.frame(mtcars)
-?mtcars
+df = as.data.frame(Auto)
+?Auto
 # Getting an overview of the relation between each pair of variables
 pairs(df)
 
 ## Variable Types
-model = lm(mpg ~ cyl, data=df)
+model = lm(mpg ~ cylinders, data=df)
 summary(model)
 
-model_disc = lm(mpg ~ factor(cyl), data=df)
+model_disc = lm(mpg ~ factor(cylinders), data=df)
 summary(model_disc)
 
 str(df)
-df$cyl = factor(df$cyl)
+df$cyl = factor(df$cylinders)
 str(df)
 
-model_disc = lm(mpg ~ cyl, data=df)
+model_disc = lm(mpg ~ cylinders, data=df)
 summary(model_disc)
 
-df = as.data.frame(mtcars)
+df = as.data.frame(Auto)
 
 #vectors (atomic)
 #4 types of vectors
@@ -48,21 +48,21 @@ as.double(c("1","1.5","2"))
 # Interaction terms
 
 # Scatterplot between mpg and hp, color coding by cyl
-ggplot(df, aes(x = hp, y = mpg, color = cyl)) + geom_point()
-ggplot(df, aes(x = hp, y = mpg, color = factor(cyl))) + geom_point()
+ggplot(df, aes(x = horsepower, y = mpg, color = cylinders)) + geom_point()
+ggplot(df, aes(x = horsepower, y = mpg, color = factor(cylinders))) + geom_point()
 
 # Linear regression model without interaction
-model = lm(mpg ~ hp + cyl, data=df)
+model = lm(mpg ~ horsepower + cylinders, data=df)
 summary(model)
 # Model with interaction
-model_int = lm(mpg ~ hp*cyl, data=df)
+model_int = lm(mpg ~ horsepower*cylinders, data=df)
 summary(model_int)
 
 # Higher order terms
-model_sq = lm(mpg ~ hp + I(hp^2), data=df)
+model_sq = lm(mpg ~ horsepower + I(horsepower^2), data=df)
 summary(model_sq)
 # General polynomials
-model_sq = lm(mpg ~ poly(hp,degree=2,raw=TRUE), data=df)
+model_sq = lm(mpg ~ poly(horsepower,degree=2,raw=TRUE), data=df)
 summary(model_sq)
 
 ## Predictions
@@ -70,8 +70,8 @@ summary(model_sq)
 fit_val = predict(model,df)
 fit_val
 # Predicted values for new data
-new_data = data.frame(hp=c(300,200,100),
-                      cyl=c(8,6,4))
+new_data = data.frame(horsepower=c(300,200,100),
+                      cylinders=c(8,6,4))
 new_data
 new_preds = predict(model,new_data)
 new_preds
@@ -79,9 +79,8 @@ new_preds
 
 ### KNN ###
 # Regression using knn.reg
-model_knn = knn.reg(train=df[,c('hp','cyl')],y=df$mpg,k=5)
+model_knn = knn.reg(train=df[,c('horsepower','cylinders')],y=df$mpg,k=5)
 model_knn
-
 # Running knn.reg for many values of k
 
 ## For loops
@@ -89,32 +88,42 @@ for (i in 1:5){
     print(i)
 }
 
+i_vect = c()
 for (i in 1:5){
-    odd = (i %% 2)==1
-    if (odd){
-        print(paste0(i," is odd"))
-    } else {
-        print(paste0(i," is even"))
-    }
+    i_vect = c(i_vect,i)
+    print(i)
 }
+i_vect
 
-# Running knn.reg, storing LOO Predictive R^2
+# Running knn.reg, storing LOO Predictive R^2 for different values of k
 r_sq = c()
-for (i in 1:10){
-    r2pred = knn.reg(train=df[,c('hp')],y=df$mpg,k=i)$R2
+for (i in 1:15){
+    r2pred = knn.reg(train=df$horsepower,y=1/df$mpg,k=i)$R2Pred
     r_sq = c(r_sq,r2pred)
 }
 r_sq
+which.max(r_sq)
+
+ks = c(3,5,7,9,11,13,15)
+r_sq = c()
+for (i in ks){
+    r2pred = knn.reg(train=df$horsepower,y=1/df$mpg,k=i)$R2Pred
+    r_sq = c(r_sq,r2pred)
+}
+r_sq
+optk = ks[which.max(r_sq)]
+optk
 
 ## Plotting knn predictions
 # Creating grid of x points
-x_grid = seq(min(df$hp),max(df$hp),0.1)
+df$gpm = 1/df$mpg
+x_grid = seq(min(df$horsepower),max(df$horsepower),0.1)
 x_grid
-x_grid_df = data.frame(hp=x_grid)
+x_grid_df = data.frame(horsepower=x_grid)
 x_grid_df
 # Getting predictions for each point in grid
-grid_pred = knn.reg(train=df[,c('hp')],y=df$mpg,k=1,test=x_grid_df)$pred
-grid_pred
-# Plotting prediction curve onto scatterplot
-ggplot(df, aes(x = hp, y = mpg)) + geom_point()
-    + geom_line(color='red', aes(x=x_grid, y=grid_pred))
+x_grid_df$pred = knn.reg(train=df$horsepower,y=df$gpm,k=optk,test=x_grid_df)$pred
+# Plotting prediction curve onto scatterplot, with linear regression curve
+ggplot(df, aes(x = horsepower, y = gpm)) + geom_point() +
+    geom_line(color='red', data=x_grid_df, aes(x=horsepower, y=pred)) +
+    geom_smooth(method='lm',formula='y ~ x + I(x^2)')
