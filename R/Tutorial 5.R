aa3c9b20491a26766a4b7436cf6e3878261eecc4
Mehtab Hanzroh - SP6 <mehtabhanzroh@gmail.com>
2025-02-13 14:31:20 -0500

    Tutorial 5 R Code Adjustments

--- R/Tutorial 5.R
+++ R/Tutorial 5.R
@@ -7,7 +7,7 @@ library(ISLR)
 library(tidyverse)
 library(rsample)
 library(glmnet)
-# pls library for principal components
+# pls library for principal component regression
 library(pls)
 # splines library for cubic and smoothing splines
 library(splines)
@@ -61,57 +61,94 @@ mean((pca_pred - df_train[,'mpg'])^2)
 
 # Base R
 dens = density(df$mpg, kernel='epanechnikov')
-plot(dens$x,dens$y)
+hist(df$mpg,freq=FALSE)
+lines(dens$x,dens$y)
+
+dens2 = density(df$mpg, kernel='epanechnikov', adjust=0.5)
+hist(df$mpg,freq=FALSE)
+lines(dens$x,dens$y)
+lines(dens2$x,dens2$y,col='blue')
+
+dens3 = density(df$mpg, kernel='epanechnikov', adjust=1.5)
+hist(df$mpg,freq=FALSE)
+lines(dens$x,dens$y)
+lines(dens2$x,dens2$y,col='blue')
+lines(dens3$x,dens3$y,col='red')
+
 
 # ggplot
-ggplot(data=df,mapping=aes(x=mpg)) + geom_density(kernel='epanechnikov')
-
-# with histogram
 ggplot(data=df,mapping=aes(x=mpg)) + geom_histogram(aes(y = ..density..)) + 
-    geom_density(kernel='epanechnikov')
+  geom_density(kernel='epanechnikov') + 
+  geom_density(kernel='epanechnikov',adjust=0.5,color='blue') +
+  geom_density(kernel='epanechnikov',adjust=1.5,color='red')
 
 ### Basis Functions ###
 
 ## Generate Data ##
 
-n  <- 101                                                                             # We want 101 observations
-x  <- seq(0, 1, length.out = n)                                                       # Defining variable x
-fx <- sin(2 * pi * x)                                                                 # Defining the DGP
-y  <- fx + rnorm(n, sd = 0.5)                                                         # Adding random noise
-plot(x, y)                                                                            # Initiating the plot environment 
-lines(x, fx, lwd = 1, col = "black")                                                  # Plotting f(x)
-legend("topright", legend = "f(x)",                                                   # Defining hhe plot legend  
-       lty = 1, lwd = 2, bty ="n")                                                    # bty = "n" dictates the style of the legend box
+n  = 101
+x  = seq(0, 1, length.out = n)
+fx = sin(2 * pi * x)
+set.seed(1234)
+y = fx + rnorm(n, sd = 0.5)
+
+plot(x, y, col = 'azure4')
+lines(x, fx, lwd = 1, col = "black")
+legend("topright", legend = "f(x)", lty = 1, lwd = 2)
 
 ## Polynomials ##
-lm_poly   <- lm(y ~ x + I(x^2) + I(x^3))                                              # Fitting a polynomial regressions
-poly_pred <- predict(lm_poly, se = TRUE)                                              # Predicting using the fitted model
-plot(x, y, ylab = "y")                                                                # Initiating the plot environment 
-lines(x, predict(lm_poly),                                                            # Plotting the fit
-      lty = 1, lwd = 2, col = "blue")                                                 # Using the color blue to differentiate 
-lines(x, fx, lwd = 2, col = "black")                                                  # Plotting f(x)
-legend("topright",                                                                    # Setting the legend on the top right of the plot
-       legend = c("f(x)", "Polynomial"), lty = 1, lwd = 2,                            # We want a solid line, with higher width
-       col = c("blue", "black"), bty ="n")                                            # Specifying the type of legend box and label colors
+lm_poly = lm(y ~ x + I(x^2) + I(x^3))
+poly_pred = predict(lm_poly, se = TRUE)
+plot(x, y, col = 'azure4', ylab = "y")
+lines(x, predict(lm_poly), lty = 1, lwd = 2, col = "blue")
+lines(x, fx, lwd = 2, col = "black")
+legend("topright", legend = c("f(x)", "Polynomial"), lty = 1, lwd = 2,
+       col = c("blue", "black"))
 
 ## Splines ##
-k     <- c(0.25, 0.815)                                                               # Adding knots by visual inspection
-lm_spline <- lm(y ~ bs(x, knots = k) - 1)                                             # Fitting the cubic spline
-c_pred     <- predict(lm_spline, se = T)                                              # Getting the predictions
-plot(x, y, ylab = "y")                                                                # Initiating the plot environment
-lines(x, fx, lwd = 2, col = "black")                                                  # Plotting f(x) against x
-lines(x, predict(lm_poly), lty = 1, lwd = 2, col = "blue")                            # Assigning it the color blue
-lines(x, fitted(lm_spline), lwd = 2, col = "red")                                     # Assigning it the color red
-legend("topright", legend = c("f(x)", "Polynomial", "Cubic"),                         # Adding the cubic spline label
-       lty = 1, lwd = 2, col = c("black", "blue", "red"), bty = "n")                  # Specifying the style of legend box
+
+# Selecting knots by visual inspection
+k = c(0.25, 0.8)
+# Basis Splines
+lm_bspline = lm(y ~ bs(x, knots = k) - 1)
+bs_pred = predict(lm_bspline, se = T)
+plot(x, y, col = 'azure4', ylab = "y")
+lines(x, fx, lty = 1, lwd = 2, col = "black")
+lines(x, fitted(lm_bspline), lwd = 2, col = "red")
+# Natural Splines
+k2 = c(0.25,0.5,0.8)
+lm_nspline = lm(y ~ ns(x, knots = k) - 1)
+lm_nspline2 = lm(y ~ ns(x, knots = k2) - 1)
+ns_pred = predict(lm_nspline, se = T)
+plot(x, y, col = 'azure4', ylab = "y")
+lines(x, fx, lty = 1, lwd = 2, col = "black")
+lines(x, fitted(lm_bspline), lwd = 2, col = "red")
+lines(x, fitted(lm_nspline), lwd = 2, col = "orange")
+lines(x, fitted(lm_nspline2), lwd = 2, col = "green")
+legend("topright", legend = c("B-Spline", "N-Spline (2 Knots)", "N-Spline (3 Knots)"),
+       lty = 1, lwd = 2, col = c("red", "orange", "green"))
 
 ## Smoothing Splines ##
-smth_spline <- smooth.spline(x, y, nknots = 10)                                       # Fitting the smoothing spline, can use spar instead of number of knots
-plot(x, y, ylab = "y")                                                                # Initiating the plot environment
-lines(x, fx, lty = 1, lwd = 2, col = "black")                                         # Plotting f(x)
-lines(x, predict(lm_poly), lty = 1, lwd = 2, col = "blue")                            # Plotting the polynomial regressions
-lines(x, fitted(lm_spline), lty = 1, lwd = 2, col = "red")                            # Plotting the cubic spline
-lines(x, smth_spline$y, lty = 1, lwd = 2, col = "green")                              # Plotting the smoothing spline
-legend("topright",                                                                    # We want the legend on the top right 
-       legend = c("f(x)", "Polynomial", "Cubic Spline", "Smoothing Spline"),          # Specifying the labels
-       lty = 1, lwd = 2, col = c("black", "blue", "red", "green"))                    # Specifying the colors
+smth_spline = smooth.spline(x, y, nknots = 10)
+plot(x, y, col = 'azure4', ylab = "y")
+lines(x, fx, lty = 1, lwd = 2, col = "black")
+lines(x, smth_spline$y, lty = 1, lwd = 2, col = "purple")
+
+smth_spline2 = smooth.spline(x, y)
+plot(x, y, col = 'azure4', ylab = "y")
+lines(x, fx, lty = 1, lwd = 2, col = "black")
+lines(x, smth_spline$y, lty = 1, lwd = 2, col = "purple")
+lines(x, smth_spline2$y, lty = 1, lwd = 2, col = "cyan")
+
+
+# Plotting all prediction lines
+plot(x, y, col = 'azure4', ylab = "y")
+lines(x, fx, lty = 1, lwd = 2, col = "black")
+lines(x, predict(lm_poly), lty = 1, lwd = 2, col = "blue")
+lines(x, fitted(lm_bspline), lwd = 2, col = "red")
+lines(x, fitted(lm_nspline), lwd = 2, col = "orange")
+lines(x, fitted(lm_nspline2), lwd = 2, col = "green")
+lines(x, smth_spline$y, lty = 1, lwd = 2, col = "purple")
+lines(x, smth_spline2$y, lty = 1, lwd = 2, col = "cyan")
+legend("bottomleft", legend = c("f(x)", "Polynomial", "B-Spline", "N-Spline (2 Knots)", "N-Spline (3 Knots)", "Smoothing Spline (10 Knots)", "Smoothing Spline"),
+       lty = 1, lwd = 2, col = c("black", "blue", "red", "orange", "green", "purple", "cyan"))
